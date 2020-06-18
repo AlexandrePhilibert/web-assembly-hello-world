c5ba1eef685da832d33d1f24afbcb13728251c51
Alexandre Philibert <philibert.alexandre@gmail.com>
2020-06-18 22:33:57 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// https://rustwasm.github.io/book/game-of-life/implementing.html
+// https://rustwasm.github.io/docs/wasm-bindgen/examples/dom.html
+#[wasm_bindgen(start)]
+pub fn run() -> Result<(), JsValue> {
+
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+
+    let val = document.create_element("p")?;
+    val.set_inner_html("Hello from Rust!");
+
+    body.append_child(&val)?;
+
+    Ok(())
+}
